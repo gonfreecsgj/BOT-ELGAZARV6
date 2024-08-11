696128ced57ff928651faac9827ad020f4d05d9e
gonfreecsgj <gonf9824@gmail.com>
2024-08-11 13:51:29 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,14 +22,14 @@ global.socialm = "GitHub: ELGAZAR" //ur github or insta name
 global.location = "Africa, Egypt, Cairo" //ur location
 
 //bot bomdy 
-global.ownernomer "967734394867" //ur number
-global.premium = ['201098906252'] //ur premium number
+global.ownernomer "+967734394867" //ur number
+global.premium = ['967734394867'] //ur premium number
 global.botname = '❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️' //ur bot name
 global.linkz = "https://youtube.com/@ABDALLAH_MOHAMED" //your theme url which will be displayed on whatsapp
 global.websitex = "https://telegra.ph/file/ec763cc3e7c88d0e67682.jpg" //ur website to be displayed
 global.botscript = 'https://youtube.com/@ABDALLAH_MOHAMED' //script link
 global.themeemoji = "♥" //ur theme emoji
-global.packname = "⍣الجزار الوزير يروحي⍣" //ur sticker watermark packname
+global.packname = "⍣كوثر ميكاسا" //ur sticker watermark packname
 global.author = "⍣967734394867⍣" //ur sticker watermark author
 global.wm = "❤️mikasa ᗷOT❤️." //ur watermark
 
